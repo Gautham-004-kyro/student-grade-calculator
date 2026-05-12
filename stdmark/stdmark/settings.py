13d64d8d4f143e639cb83c1599946106ba49c32b
Gautham-004 <u2403125@rajagiri.edu.in>
2026-05-12 18:17:55 +0530

    Added Promailer SMTP

--- stdmark/stdmark/settings.py
+++ stdmark/stdmark/settings.py
@@ -260,16 +260,29 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
 
-EMAIL_PORT = 587
+EMAIL_PORT = int(
+    os.environ.get(
+        "EMAIL_PORT",
+        587
+    )
+)
 
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_USE_TLS = True 
 
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = os.environ.get(
+    "EMAIL_HOST_USER"
+)
 
+EMAIL_HOST_PASSWORD = os.environ.get(
+    "EMAIL_HOST_PASSWORD"
+)
 
+DEFAULT_FROM_EMAIL = (
+    f"{os.environ.get('EMAIL_FROM_NAME')} "
+    f"<{EMAIL_HOST_USER}>"
+)
 
