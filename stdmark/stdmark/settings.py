49a4bc0e8b584c5ad9c1a6e58cf729a44643dc3e
Gautham-004 <u2403125@rajagiri.edu.in>
2026-05-12 13:24:41 +0530

    updated settings.py

--- stdmark/stdmark/settings.py
+++ stdmark/stdmark/settings.py
@@ -238,9 +238,9 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "gowthampramod005@gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
-EMAIL_HOST_PASSWORD = "rjxj uewx jhld dvbw"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 
