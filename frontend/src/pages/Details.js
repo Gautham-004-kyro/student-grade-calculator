c68c58f3ef03b64fca2a1a64990a52fb05bb0246
Gautham-004 <u2403125@rajagiri.edu.in>
2026-05-12 12:03:54 +0530

    updated PAGINATION

--- frontend/src/pages/Details.js
+++ frontend/src/pages/Details.js
@@ -407,6 +407,47 @@ const updateStudent = async () => {
         </tbody>
 
       </table>
+
+      <div className="pagination">
+
+  <button
+
+    disabled={currentPage === 1}
+
+    onClick={() =>
+      setCurrentPage(currentPage - 1)
+    }
+  >
+
+    Prev
+
+  </button>
+
+
+
+  <span>
+
+    Page {currentPage} of {totalPages}
+
+  </span>
+
+
+
+  <button
+
+    disabled={currentPage === totalPages}
+
+    onClick={() =>
+      setCurrentPage(currentPage + 1)
+    }
+  >
+
+    Next
+
+  </button>
+
+</div>
+
         {showEditPopup && (
 
   <div className="popup-overlay">
