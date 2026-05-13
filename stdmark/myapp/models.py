cc6a9f44acee5ed3719f92fa12e96449695330e5
Gautham-004 <u2403125@rajagiri.edu.in>
2026-05-13 15:57:28 +0530

    migrate to neon db

--- stdmark/myapp/models.py
+++ stdmark/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Student(models.Model):
-    name = models.CharField(max_length=100) 
+    name = models.CharField(max_length=100, unique=True) 
     subject1 = models.IntegerField()
     subject2 = models.IntegerField()
     subject3 = models.IntegerField()
