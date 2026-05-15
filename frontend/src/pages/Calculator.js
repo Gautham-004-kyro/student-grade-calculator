d9687ec39e1e700829fd3aae39fc077465b5890b
Gautham-004 <u2403125@rajagiri.edu.in>
2026-05-15 12:14:41 +0530

    fix auth imports

--- frontend/src/pages/Calculator.js
+++ frontend/src/pages/Calculator.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
